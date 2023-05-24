426a71ff54e6dbcdda70ce982fbe203c63129aad
Alex Janka <alex@alexjanka.com>
2023-05-24 16:41:03 +1000

    memcpy

--- boards/waveshare-rp2040-zero/src/lib.rs
+++ boards/waveshare-rp2040-zero/src/lib.rs
@@ -13,7 +13,8 @@ pub use hal::entry;
 #[link_section = ".boot2"]
 #[no_mangle]
 #[used]
-pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
+pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_RAM_MEMCPY;
+// pub static BOOT2_FIRMWARE: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 
 pub use hal::pac;
 
